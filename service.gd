c1a86b48215245e31d16534f55f8b953d72278bb
Weiner Oliveira <woliveira82@gmail.com>
2024-02-06 21:41:02 +0000

    feat: handle simultaneous requests

--- service.gd
+++ service.gd
@@ -1,24 +1,18 @@
 extends Node
 
-signal finished
-
 var _queue: Array
 var _response
 
-@onready var _http_request
-
 
 func post_request(request, handle_function: Callable):
-	self.finished.connect(handle_function)
 	self._response = null
-	self._http_request = HTTPRequest.new()
-	add_child(self._http_request)
-	self._http_request.request_completed.connect(self._on_request_completed)
-	self._http_request.request(request.url)
-	await self._http_request.request_completed
-	self.finished.emit(self._response)
-	self.finished.disconnect(handle_function)
-	self._http_request.queue_free()
+	var instance = HTTPRequest.new()
+	add_child(instance)
+	instance.request_completed.connect(self._on_request_completed)
+	instance.request(request.url)
+	await instance.request_completed
+	handle_function.call(self._response)
+	instance.queue_free()
 
 
 func _on_request_completed(result, response_code, headers, body):
