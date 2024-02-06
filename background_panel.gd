570aed1d56b9a51f870fe1f7d486c764abb85e95
Weiner Oliveira <woliveira82@gmail.com>
2024-02-06 20:27:01 +0000

    feat: reuse same request node

--- background_panel.gd
+++ background_panel.gd
@@ -1,9 +1,15 @@
 extends Panel
 
+@onready var rich_text := $VBoxContainer/RichTextLabel
+
 
 func _on_button_pressed():
 	var request = {
 		"method": "GET",
 		"url": "https://catfact.ninja/fact?max_length=100",
 	}
-	Service.post_request(request)
+	Service.post_request(request, self._handle)
+
+
+func _handle(response):
+	self.rich_text.text = response.fact
--- service.gd
+++ service.gd
@@ -4,9 +4,13 @@ signal finished
 
 var _queue: Array
 var _busy: bool = false
+var _response
+
+@onready var _http_request := $HTTPRequest
 
 
-func post_request(request):
+func post_request(request, handle_function: Callable):
+	self.finished.connect(handle_function)
 	self._queue.push_back(request)
 	if not _busy:
 		self._handle_next_request()
@@ -22,14 +26,19 @@ func _handle_next_request():
 
 func _handle(request):
 	self._busy = true
-	# do stuff
+	self._response = null
 	
-	var instance = HTTPRequest.new()
-	instance.request(request.url)
-	await instance.request_completed
+	self._http_request.request_completed.connect(self._on_request_completed)
+	self._http_request.request(request.url)
+	await self._http_request.request_completed
 	
-	
-	
-	# do stuff
 	self._busy = false
-	self.finished.emit()
+	self.finished.emit(self._response)
+
+
+func _on_request_completed(result, response_code, headers, body):
+	if response_code != 200:
+		push_error("ERROR: Response status = %d" % response_code)
+		return
+	
+	self._response = JSON.parse_string(body.get_string_from_utf8())
